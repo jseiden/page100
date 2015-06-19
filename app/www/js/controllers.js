49210b8ace609580d9eaaddc304e354d9baaf618
Danielle Blank <dgdblank@gmail.com>
2015-06-18 20:00:09 -0700

    Secondary button on main nav for stack route. Created 'go' method in AppCtrl to navigate to Stack.

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -3,13 +3,17 @@
 
 angular.module("starter.controllers", [])
 
-.controller("AppCtrl", function($scope, $ionicModal, $timeout) {
+.controller("AppCtrl", function($scope, $ionicModal, $timeout, $location) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //$scope.$on("$ionicView.enter", function(e) {
   //});
+
+  $scope.go = function ( path ) {
+    $location.path( path );
+  };
   // Form data for the login modal
   $scope.loginData = {};
 

f13667a03daff1c2671a129a54636f0a1094a8c6
Valerie Liang <liang.val63@gmail.com>
2015-06-19 16:43:33 -0700

    Added FE structure for authentication

---
+++ app/www/js/Auth/auth.js
@@ -0,0 +1,37 @@
+"use strict";
+angular.module("starter.auth", [])
+
+.controller("AuthCtrl", function($scope, Auth, $location, $window){
+  $scope.user = {};
+
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem("com.starter", token);
+        $location.path("/stack");
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem("com.starter", token);
+        $location.path("/stack");
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  $scope.signout = function () {
+    $window.localStorage.removeItem("com.starter");
+    $location.path("/login");
+  };
+
+  $scope.isAuth = function(){
+    return !!$window.localStorage.getItem("com.starter");
+  };
+});

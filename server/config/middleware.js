9cee5ba69b1b7241d2779471901440c67a8c84a6
jseiden <jseiden@gmail.com>
2015-06-23 03:50:09 -0700

    add middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,17 +1,28 @@
 "use strict";
 
 var morgan = require("morgan");
+var bodyParser = require("body-parser");
+var tokenHandler = require("./helpers.js");
 
 module.exports = function (app, express) {
+
+  //TODO: figure out if express.Router is still needed. mixed options online and works as is
   // var userRouter = express.Router();
   // var bookRouter = express.Router();
 
-
   //logs http requests
   app.use(morgan("dev"));
+  //parses body of http request
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+ //serves static files
+ //TODO jslint doesn't like '+' but haven't gotten path.join to work 
+  app.use(express.static(__dirname + "./../../app/www"));
+
 
   // app.use('/users', userRouter);
   // app.use('/books', bookRouter);
+  app.use("./../User", tokenHandler.decode);
 
   require("../User/userRoutes.js")(app, express);
   require("../Book/bookRoutes.js")(app, express);
--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 "use strict";
 var express = require("express");
-// var mongoose = require("mongoose");
 
+//orm to connect to mongodb
+var mongoose = require("mongoose");
+
+//instance of express
 var app = express();
 
-// mongoose.connect('mongodb://localhost/shortly');
+//connect to db with mongoose ORM
+//TODO: change to Digital Ocean Url
+mongoose.connect("mongodb://localhost/page100");
 
-// configure our server with all the middleware and and routing
+// configure our server with all the middleware and routing
 require("./config/middleware.js")(app, express);
 
+//port defaults to localhost3000 if env variable is not present
 var port = process.env.port || 3000;
 
 app.listen(port, function() {

f4da72e0b27a1eeeff19b19c28505ab8f940d81f
Valerie Liang <liang.val63@gmail.com>
2015-06-27 12:06:37 -0700

    fixed card dragging

--- app/www/js/Card/card.js
+++ app/www/js/Card/card.js
@@ -2,8 +2,9 @@
 
 angular.module("starter.cards", [])
 
-.controller("CardsCtrl", function($scope, BookChoices, $rootScope){
-  // var cardTypes = [{image: "http://ecx.images-amazon.com/images/I/81eMMNVkt7L.jpg", title: "East of Eden", author: "John Steinbeck"}, {image: "http://ecx.images-amazon.com/images/I/81mEbZQOrXL.jpg", title: "Great Gatsby", author: "F. Scott Fitzgerald"}, {image: "http://ecx.images-amazon.com/images/I/51tkcSAhSDL.jpg", title: "100 Years of Solitude", author: "Gabriela Garcia Marquez"}];
+.controller("CardsCtrl", function($scope, BookChoices, $ionicSideMenuDelegate, $rootScope){
+  //prevent side menu from dragging out with cards
+  $ionicSideMenuDelegate.canDragContent(false);
 
   $scope.getBooks = function(userId, count){
     BookChoices.getBooks(userId, count)
--- app/www/js/app.js
+++ app/www/js/app.js
@@ -37,7 +37,7 @@ angular.module("starter", [
   //       if(isLoggedIn){
   //         deferred.resolve();
   //       } else {
-  //         deferred.reject('Not logged in');
+  //         deferred.reject("Not logged in");
   //       }
   //     });
   //     return deferred.promise;
@@ -144,7 +144,7 @@ angular.module("starter", [
 .factory("AttachTokens", function ($window) {
   // this is an $httpInterceptor
   // its job is to stop all out going request
-  // then look in local storage and find the user's token
+  // then look in local storage and find the user"s token
   // then add it to the header so the server can validate the request
   var attach = {
     request: function (object) {
@@ -159,14 +159,14 @@ angular.module("starter", [
   return attach;
 })
 
-.run(["$rootScope", "$state", function ($rootScope, $state) {
+.run(["$rootScope", "$state", "$ionicSideMenuDelegate", function ($rootScope, $state, $ionicPlatform, $ionicSideMenuDelegate) {
 //   // here inside the run phase of angular, our services and controllers
 //   // have just been registered and our app is ready
 //   // however, we want to make sure the user is authorized
 //   // we listen for when angular is trying to change routes
 //   // when it does change routes, we then look for the token in localstorage
-//   // and send that token to the server to see if it is a real user or hasn't expired
-//   // if it's not valid, we then redirect back to signin/signup
+//   // and send that token to the server to see if it is a real user or hasn"t expired
+//   // if it"s not valid, we then redirect back to signin/signup
   $rootScope.$on("$stateChangeStart", function (event, toState) {
     var requireLogin = toState.data.requireLogin;
       if(requireLogin && !$rootScope.currentUser){
@@ -190,6 +190,7 @@ angular.module("starter", [
   // $rootScope.$on("stateChangeError", function (){
   //   $state.go("signin");
   });
+
 }]);
 
 //   });

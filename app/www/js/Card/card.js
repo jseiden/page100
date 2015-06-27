124e27549c5b3cce888db8293dff959b32b9caff
Chris <claymanchris@gmail.com>
2015-06-26 20:15:39 -0700

    fix linting

--- app/www/js/Card/card.js
+++ app/www/js/Card/card.js
@@ -3,8 +3,6 @@
 angular.module("starter.cards", [])
 
 .controller("CardsCtrl", function($scope, BookChoices, $rootScope){
-  var cardTypes;
-
   // var cardTypes = [{image: "http://ecx.images-amazon.com/images/I/81eMMNVkt7L.jpg", title: "East of Eden", author: "John Steinbeck"}, {image: "http://ecx.images-amazon.com/images/I/81mEbZQOrXL.jpg", title: "Great Gatsby", author: "F. Scott Fitzgerald"}, {image: "http://ecx.images-amazon.com/images/I/51tkcSAhSDL.jpg", title: "100 Years of Solitude", author: "Gabriela Garcia Marquez"}];
 
   $scope.getBooks = function(userId, count){
@@ -26,11 +24,11 @@ angular.module("starter.cards", [])
     console.log("Right swipe", index);
     // TO DO: Check data passed to addToStack, e.g. index or entire book data
     BookChoices.addToStack($scope.userId, $scope.cards[index]);
-    $scope.currentCard = $scope.cards[index+1];
+    $scope.currentCard = $scope.cards[index + 1];
   };
 
   $scope.cardDestroyed = function(index) {
-    $scope.currentCard = $scope.cards[index+1];
+    $scope.currentCard = $scope.cards[index + 1];
     console.log("Card removed");
   };
 
